9973f96f075fd93e6864c567f452c86c38cc2145
NiGoCodes <169056827+NiGoCodes@users.noreply.github.com>
2025-07-15 11:46:45 +0530

    updating app.tsx

--- Logging Middlewares/logger.ts
+++ Logging Middlewares/logger.ts
@@ -1,3 +1,20 @@
 export type Stack = "frontend";
 export type Level = "debug" | "info" | "warn" | "error" | "fatal";
 export type Package = "api" | "utils" | "config" | "middleware" | "auth";
+
+export async function log(
+  stack: Stack,
+  level: Level,
+  pkg: Package,
+  message: string
+) {
+  try {
+    await fetch("http://20.244.56.144/evaluation-service/logs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ stack, level, package: pkg, message })
+    });
+  } catch (err) {
+    
+  }
+}
